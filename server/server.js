dc37eb772aee62c4dc590bc5a29eb6833bb46268
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-10-06 16:19:45 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
 mongoose.connect(process.env.MONGO_URI,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
